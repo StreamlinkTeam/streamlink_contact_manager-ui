7deaac282a45ed1ba19c13db19e6e68d80c83c93
chemakh <lazher.chemakh@gmail.com>
2018-07-24 18:17:26 +0200

    add features

---
+++ src/app/developer/developer.component.ts
@@ -0,0 +1,28 @@
+import {Developer} from '../shared/entities/developer.model';
+import {User} from '../shared/entities/user.model';
+import {Component} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'developer.component.html'
+})
+export class DeveloperComponent {
+
+  editing = false;
+  referenceDeveloper: string;
+
+
+  constructor(private router: Router,
+    activeRoute: ActivatedRoute) {
+    this.editing = activeRoute.snapshot.params['mode'] === 'edit';
+
+    if (this.editing) {
+      this.referenceDeveloper = activeRoute.snapshot.params['reference'];
+    } else {
+      this.referenceDeveloper = null;
+    }
+  }
+
+
+}
