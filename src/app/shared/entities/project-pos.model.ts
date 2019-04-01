33e04fddb04df313cd9122fade90ab57d9fd8545
HarounGazzah <haroun.gazzah@gmail.com>
2019-04-01 20:46:09 +0100

    adding ProjectView Model

---
+++ src/app/shared/entities/project-pos.model.ts
@@ -0,0 +1,28 @@
+export class ProjectPos {
+
+  reference: any;
+  resourceReference: any;
+  needReference: any;
+  responsibleReference: any;
+  client: any;
+  needTitle: any;
+  resourceFullName: any;
+  stage: any;
+  startDate: any;
+  endDate: any;
+  tjm: any;
+  cjm: any;
+  invoicedDays: any;
+  freeDays: any;
+  note: any;
+  createdDate: any;
+  modifiedDate: any;
+  place: any;
+  comment: any;
+  currency: any;
+  presentationDate: any;
+  periodMargin: any;
+  periodCA: any;
+  periodCost: any;
+  periodProfitability: any;
+}
