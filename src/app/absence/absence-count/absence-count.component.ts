abc18df482befee0d76c762fcc2e0426a102b11b
Takwa <takwa.rabhi@esprit>
2019-07-23 16:30:55 +0100

    ajout composant absence

--- src/app/absence/absence-count/absence-count.component.ts
+++ src/app/absence/absence-count/absence-count.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DeveloperService} from '../../shared/services/developer.service';
+import {AbsenceService} from '../../shared/services/AbsenceService';
+import {UserService} from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-absence-count',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./absence-count.component.css']
 })
 export class AbsenceCountComponent implements OnInit {
+  devoloper: any;
+  actuelDate = new Date();
+  countMonths: any;
 
-  constructor() { }
+  constructor(private developerService: DeveloperService,
+              private  absenceService: AbsenceService,
+              private userService: UserService) {
+  }
+
+
+
+
 
   ngOnInit() {
+    this.loadDeveloper();
+
+
+    return this.developerService.getCurrentLeave().subscribe(res => {
+      console.log(res);
+      this.countMonths = res ;
+    });
   }
+
+  loadDeveloper() {
+    return this.developerService.getCurrentDeveloper().subscribe((data: {}) => {
+      this.devoloper = data;
+
+    });
+  }
+
+
 
 }
